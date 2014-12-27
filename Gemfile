e4c6fb8bd880105d0bc1c963147c6b32200c4408
Germain Boue <boue.germain@gmail.com>
2014-12-27 11:44:22 -0600

    Able to add a picture with the paperclip gem on each recipe

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'simple_form', '~> 3.1.0'
-# gem 'paperclip', '~> 4.2.1'
+gem 'paperclip', '~> 4.2.1'
 gem 'bootstrap-sass', '~> 3.3.1.0'
 gem 'haml', '~> 4.0.6'
 gem 'sdoc', '~> 0.4.0',          group: :doc
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141227170345) do
+ActiveRecord::Schema.define(version: 20141227174039) do
 
   create_table "recipes", force: true do |t|
     t.string   "title"
